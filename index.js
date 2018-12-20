dde125747791c691e03794dfde56903e56589018
Shubham Ingale <singale864@gmail.com>
2018-12-20 15:26:11 +0530

    Commit

---
+++ index.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+    $("#hide").click(function(){
+        $("p").hide(1000);
+    });
+    $("#show").click(function(){
+        $("p").show(500);
+    });
+    $("#toggle").click(function(){
+        $("p").toggle(500);
+    })
+
+    // fade example
+
+    $("#fadein").click(function(){
+        $("#fadeDiv").fadeIn(1000); //can use 'slow' instead of time(1000)
+    });
+    $("#fadeout").click(function(){
+        $("#fadeDiv").fadeOut();
+    });
+    $("#fadeToggle").click(function(){
+        $("#fadeDiv").fadeToggle('slow');
+    });
+    $("#fadeto").click(function(){
+        $("#fadeDiv").fadeTo('slow',0.5);
+    })
+
+});
